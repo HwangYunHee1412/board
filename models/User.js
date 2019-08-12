d7fe62d2ed172cb5932a72e4288e1d145ded0131
HwangYunHee1412 <gold831109@naver.com>
2019-08-12 15:55:56 +0900

    last commit!

--- models/User.js
+++ models/User.js
@@ -3,10 +3,29 @@ var bcrypt = require("bcrypt-nodejs"); //
 
 // schema // 1
 var userSchema = mongoose.Schema({
- username:{type:String, required:[true,"Username is required!"], unique:true},
- password:{type:String, required:[true,"Password is required!"], select:false},
- name:{type:String, required:[true,"Name is required!"]},
- email:{type:String}
+  username:{
+   type:String,
+   required:[true,"Username is required!"],
+   match:[/^.{4,12}$/,"Should be 4-12 characters!"],
+   trim:true,
+   unique:true
+  },
+  password:{
+   type:String,
+   required:[true,"Password is required!"],
+   select:false
+  },
+  name:{
+   type:String,
+   required:[true,"Name is required!"],
+   match:[/^.{4,12}$/,"Should be 4-12 characters!"],
+   trim:true
+  },
+  email:{
+   type:String,
+   match:[/^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+.[a-zA-Z]{2,}$/,"Should be a vaild email address!"],
+   trim:true
+  }
 },{
  toObject:{virtuals:true}
 });
@@ -31,31 +50,37 @@ userSchema.virtual("newPassword")
 
 // password validation
 // virtual들은 직접 validation이 안되기 때문에(DB에 값을 저장하지 않으니까 어찌보면 당연합니다) password에서 값을 확인
+var passwordRegex = /^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,16}$/; //  8-16자리 문자열 중에 숫자랑 영문자가 반드시 하나 이상 존재해야 한다는 뜻의 regex
+var passwordRegexErrorMessage = "Should be minimum 8 characters of alphabet and number combination!";
 userSchema.path("password").validate(function(v) {
   var user = this; //this는 user model
 
  // create user
  if(user.isNew){ // DB에 한번도 기록되지 않았던 model
-  if(!user.passwordConfirmation){
-   user.invalidate("passwordConfirmation", "Password Confirmation is required!");
-  }
-  if(user.password !== user.passwordConfirmation) {
-   user.invalidate("passwordConfirmation", "Password Confirmation does not matched!");
-  }
+   if(!user.passwordConfirmation){
+      user.invalidate("passwordConfirmation", "Password Confirmation is required!");
+    }
+    if(!passwordRegex.test(user.password)){
+      user.invalidate("password", passwordRegexErrorMessage);
+    } else if(user.password !== user.passwordConfirmation) {
+      user.invalidate("passwordConfirmation", "Password Confirmation does not matched!");
+    }
  }
 
  // update user
- if(!user.isNew){
-  if(!user.currentPassword){
-   user.invalidate("currentPassword", "Current Password is required!");
+  if(!user.isNew){
+    if(!user.currentPassword){
+      user.invalidate("currentPassword", "Current Password is required!");
+    }
+    if(user.currentPassword && !bcrypt.compareSync(user.currentPassword, user.originalPassword)){
+      user.invalidate("currentPassword", "Current Password is invalid!");
+    }
+    if(user.newPassword && !passwordRegex.test(user.newPassword)){
+      user.invalidate("newPassword", passwordRegexErrorMessage);
+    } else if(user.newPassword !== user.passwordConfirmation) {
+      user.invalidate("passwordConfirmation", "Password Confirmation does not matched!");
+    }
   }
-  if(user.currentPassword && !bcrypt.compareSync(user.currentPassword, user.originalPassword)){ // 2
-   user.invalidate("currentPassword", "Current Password is invalid!");
-  }
-  if(user.newPassword !== user.passwordConfirmation) {
-   user.invalidate("passwordConfirmation", "Password Confirmation does not matched!");
-  }
- }
 });
 // hash password // 3
 userSchema.pre("save", function (next){
